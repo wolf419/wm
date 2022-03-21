d8c04010bc07a37069d8cbfdff0514c4fb9ad683
Jakob Beck <jakob.beck@student.tugraz.at>
2022-03-21 20:34:35 +0100

    gaps

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -91,8 +91,8 @@ static Key keys[] = {
 	{ MODKEY,              			XK_x,      shiftview, 	   { .i = +1 } },
 	{ MODKEY,              			XK_y,      shiftview,  	   { .i = -1 } },
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
-	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
+	{ MODKEY,                       XK_plus,   setgaps,        {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_plus,   setgaps,        {.i = 0  } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
