c0a8a8a7a73dba41dbd4b19241f298833fc9a72e
Jakob Beck <jakob.beck@student.tugraz.at>
2022-05-11 02:21:43 +0200

    color

--- dwm-6.3/config.h
+++ dwm-6.3/config.h
@@ -7,11 +7,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=12" };
 static const char dmenufont[]       = "monospace:size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#444444";
+static const char col_gray1[]       = "#1C1B22";
+static const char col_gray2[]       = "#1C1B22";
+static const char col_gray3[]       = "#BBBBBB";
+static const char col_gray4[]       = "#EEEEEE";
+static const char col_cyan[]        = "#42414D";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
